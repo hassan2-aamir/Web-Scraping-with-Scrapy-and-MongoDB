ff3854454567fd07bfc69d22f6fc58dbbfc90fe1
hassan2-aamir <haamir.bscs23seecs@seecs.edu.pk>
2024-08-10 14:15:48 +0500

    fix database connection

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -18,5 +18,5 @@ class MongoDBPipeline(object):
                 valid = False
                 raise DropItem("Missing {0}!".format(data))
         if valid:
-            self.collection.insert(dict(item))
+            self.collection.insert_one(dict(item))
         return item
